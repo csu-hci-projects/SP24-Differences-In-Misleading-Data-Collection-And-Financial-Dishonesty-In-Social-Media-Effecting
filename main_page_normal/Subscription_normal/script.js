3529d46d90a9cef0c812168289b6c3c432d7af42
dstreeb <dstreeb@colostate.edu>
2024-04-22 10:07:08 -0600

    fix to subcribe button

--- main_page_normal/Subscription_normal/script.js
+++ main_page_normal/Subscription_normal/script.js
@@ -2,11 +2,16 @@ function subscribeNow() {
     var newPrice = "$4.99/month"; 
     document.getElementById("new-subscription-price").innerText = newPrice;
 
-    document.getElementById("confirmation-popup").style.display = 'block';
+    document.getElementById("confirmation-popup").classList.remove('hidden');
     
     document.getElementById("subscription-price").innerText = newPrice;
     document.getElementById("subscription-header").style.color = "#32CD32"; 
 }
+
+function closePopup() {
+    document.getElementById("confirmation-popup").classList.add('hidden');
+}
+
 
 function closePopup() {
     document.getElementById("confirmation-popup").style.display = 'none';
