a8abb325eb9490b3298c5ae935c8aaf12826be89
dstreeb <dstreeb@colostate.edu>
2024-04-23 10:15:50 -0600

    f

--- misdirection-in-data-collection/main_page_misleading/Subscription_misleading/script.js
+++ misdirection-in-data-collection/main_page_misleading/Subscription_misleading/script.js
@@ -6,7 +6,7 @@ function verifyIdentity() {
     var email = document.getElementById("email").value;
     var ssn = document.getElementById("ssn").value;
     if (email && ssn.length === 4) {
-        var newPrice = "$4.49/month";  // Apply 10% discount
+        var newPrice = "$4.99/month";  
         document.getElementById("new-subscription-price").innerText = newPrice;
         document.getElementById("subscription-price").innerText = newPrice;
         document.getElementById("subscription-header").style.color = "#32CD32";
